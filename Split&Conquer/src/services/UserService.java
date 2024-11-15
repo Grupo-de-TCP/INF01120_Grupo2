c8e20e5a091388c6680d04697a6c967ce0049e12
Paulo Henrique Ceccato <paulohenriquececcto@hotmail.com>
2024-11-15 11:38:20 -0300

    Teste Split&Conquer

---
+++ Split&Conquer/src/services/UserService.java
@@ -0,0 +1,18 @@
+package services;
+
+import models.User;
+import java.util.HashMap;
+import java.util.Map;
+
+//  Serviço para gerenciar usuários
+public class UserService {
+    private Map<String, User> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user.getId(), user);
+    }
+
+    public User getUser(String userId) {
+        return users.get(userId);
+    }
+}
