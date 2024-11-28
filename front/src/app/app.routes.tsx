f8048889e9d8eb63af0d317861956f2299f28cd6
Gabriel Perini <gabrielprestesperini@hotmail.com>
2024-11-28 17:46:02 -0300

    - add nav user

--- front/src/app/app.routes.tsx
+++ front/src/app/app.routes.tsx
@@ -1,19 +1,37 @@
+import { Box } from "@mui/system"
+import { NavUser } from "interface/components/navs/nav-user/nav-user.component"
 import React from "react"
-import { Navigate, useRoutes } from "react-router-dom"
+import { Navigate, Outlet, useRoutes } from "react-router-dom"
 
 const GroupsModule = React.lazy(() => import("../interface/pages/groups/groups.module"))
 
+const Layout = () => {
+  return (
+    <Box component="main">
+      <NavUser />
+      <Outlet />
+    </Box>
+  )
+}
+
 export const AppRoutes = () => {
-  const routes = useRoutes([
-    {
-      path: "/groups/*",
-      element: <GroupsModule />
-    },
-    {
-      path: "*",
-      element: <Navigate to="/groups" />
-    }
-  ])
+  const routes = useRoutes([{
+    element: <Layout />,
+    children: [
+      {
+        path: "/groups/*",
+        element: <GroupsModule />
+      },
+      {
+        path: "/debts/*",
+        element: <div />
+      },
+      {
+        path: "*",
+        element: <Navigate to="/groups" />
+      }
+    ]
+  }])
 
   return routes
 }
---
+++ front/src/interface/components/navs/nav-user/nav-user.component.tsx
@@ -0,0 +1,47 @@
+import { AppBar, Avatar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material"
+import avatarFake from "assets/avatar-fake.png"
+import { useMatch, useNavigate } from "react-router-dom"
+
+
+export const NavUser = () => {
+
+  const match = useMatch("/:tab/*")
+  const tab = match?.params.tab || "groups"
+
+  const navigate = useNavigate();
+
+  return (
+    <Box pb="104px">
+      <AppBar
+        position="fixed"
+        color="default"
+        elevation={10}
+      >
+        <Toolbar>
+          <Typography variant="h5">
+            Split&Conquer
+          </Typography>
+          <Box flexGrow={1} />
+          <Avatar
+            src={avatarFake}
+            sx={{
+              width: 40,
+              height: 40,
+            }}
+          />
+        </Toolbar>
+        <Toolbar sx={{ minHeight: "auto", justifyContent: "center" }}>
+          <Tabs
+            value={tab}
+            onChange={(_e,v) => {
+              navigate(`/${v}`)
+            }}
+          >
+            <Tab value="groups" label="Groupos" />
+            <Tab value="debts" label="Dividas" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
